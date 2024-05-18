81092783f774d07e72841315a1b7b7a27cfb7bda
crazypeace <txican@gmail.com>
2024-05-18 22:59:44 +0800

    Create blockquote2pre.js

---
+++ blockquote2pre.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // 获取所有块引用元素
+  var blockquotes = document.querySelectorAll ('blockquote');
+  
+  // 遍历每个块引用元素
+  blockquotes.forEach (function(blockquote) {
+      // 创建一个新的预备元素，并将内容复制进去
+      var pre = document.createElement ('pre');
+      pre.innerHTML  = blockquote.innerHTML ;
+  
+      // 替换原来的块引用元素为预备的pre元素
+      blockquote.parentNode.replaceChild (pre, blockquote);
+  });  
+});
