7582f04b233914ae209969ae461e131900548880
crazypeace <txican@gmail.com>
2024-06-17 01:03:52 +0800

    Update blockquote2pre.js

--- blockquote2pre.js
+++ blockquote2pre.js
@@ -22,24 +22,6 @@ document.addEventListener('DOMContentLoaded', function() {
   // 添加 复制按钮
   // 注意, 相应的需要添加CSS
   // 来源: https://yihui.org/en/2023/09/copy-button/
-  (e=>{
-      const t = e.currentScript?.dataset;
-      e.querySelectorAll(t?.selector || "pre>code").forEach((t=>{
-          const c = e.createElement("span")
-            , o = c.classList
-            , n = t.parentNode
-            , a = "CODE" === t.tagName && "PRE" === n?.tagName;
-          function s(e) {
-              o.add(e),
-              setTimeout((()=>o.remove(e)), 1e3)
-          }
-          c.className = "copy-button",
-          c.onclick = ()=>navigator.clipboard.writeText(a && t.classList.contains("code-fence") && n.parentNode?.classList.contains("fenced-chunk") ? [...n.parentNode.querySelectorAll("code[class]")].map((e=>e.innerText.replace(/\n$/, ""))).join("\n") : t.innerText).then((()=>s("copy-success")), (()=>s("copy-fail")));
-          const r = a ? n : t;
-          r.querySelector(".copy-button") || r.append(c),
-          "static" === getComputedStyle(r).position && (r.style.position = "relative")
-      }
-      ))
-  }
-  )(document);
+  // https://cdn.jsdelivr.net/npm/@xiee/utils/js/copy-button.min.js
+  (e=>{const t=e.currentScript?.dataset;e.querySelectorAll(t?.selector||"pre>code").forEach((t=>{const c=e.createElement("span"),o=c.classList,n=t.parentNode,a="CODE"===t.tagName&&"PRE"===n?.tagName;function s(e){o.add(e),setTimeout((()=>o.remove(e)),1e3)}c.className="copy-button",c.onclick=()=>navigator.clipboard.writeText(a&&t.classList.contains("code-fence")&&n.parentNode?.classList.contains("fenced-chunk")?[...n.parentNode.querySelectorAll("code[class]")].map((e=>e.innerText.replace(/\n$/,""))).join("\n"):t.innerText).then((()=>s("copy-success")),(()=>s("copy-fail")));const r=a?n:t;r.querySelector(".copy-button")||r.append(c),"static"===getComputedStyle(r).position&&(r.style.position="relative")}))})(document);
 });
