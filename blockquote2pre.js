c3cc6ae98298745e60640a64ea1b7c4e92841167
crazypeace <txican@gmail.com>
2024-06-16 13:47:39 +0800

    Update blockquote2pre.js

--- blockquote2pre.js
+++ blockquote2pre.js
@@ -1,15 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // 获取所有块引用元素
+  // 获取所有blockquote元素
   var blockquotes = document.querySelectorAll ('blockquote');
   
-  // 遍历每个块引用元素
+  // 遍历每个blockquote元素
   blockquotes.forEach (function(blockquote) {
-      // 创建一个新的预备元素，并将内容复制进去
+      // 创建一个新的code元素，并将blockquote的内容复制进去
+      var code = document.createElement ('code');    
+      code.innerHTML  = blockquote.innerHTML ;
+    
+      // 创建一个新的pre元素，并将code元素添加为其子元素
       var pre = document.createElement ('pre');
-      pre.innerHTML  = blockquote.innerHTML ;
+      pre.appendChild(code);
+    
       // 添加 class 属性
       pre.setAttribute ('class', 'prettyprint');
-      // 替换原来的块引用元素为预备的pre元素
+    
+      // 替换原来的blockquote元素为pre元素
       blockquote.parentNode.replaceChild (pre, blockquote);
   }); 
 });
