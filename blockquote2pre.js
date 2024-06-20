7591d58f100b24297b755d74114c3c249821bc21
crazypeace <txican@gmail.com>
2024-06-20 20:11:33 +0800

    Update blockquote2pre.js

--- blockquote2pre.js
+++ blockquote2pre.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
   blockquotes.forEach (function(blockquote) {
       // 创建一个新的code元素，并将blockquote的内容复制进去
       var code = document.createElement ('code');    
-      code.innerHTML  = blockquote.innerHTML;
+      code.innerHTML  = blockquote.innerHTML.replace(/<br\s*\/?>/gi, '\n');
     
       // 添加 class 属性
       code.setAttribute ('class', 'prettyprint');
